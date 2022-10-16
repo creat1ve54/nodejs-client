97dfe52b3b23f10c155a3cfa97cbefb065eddbe7
creat1ve54 <100487936+creat1ve54@users.noreply.github.com>
2022-10-16 17:58:56 +0300

    add comand all

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='https://lucent-cupcake-286300.netlify.app' element={<MainPage />} />
+        <Route path='https://lucent-cupcake-286300.netlify.app/' element={<MainPage />} />
         <Route path='https://lucent-cupcake-286300.netlify.app/adminpanel' element={<AdminPanelPage />} />
         <Route path='https://lucent-cupcake-286300.netlify.app/edit/:id' element={<EditPage />} />
         <Route path='https://lucent-cupcake-286300.netlify.app/post/:id' element={<PostItem />} />
