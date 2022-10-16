e3ec21ed5a1b5b94fcb00ebb98bd442f3f1226ab
creat1ve54 <100487936+creat1ve54@users.noreply.github.com>
2022-10-16 18:56:03 +0300

    add comand all

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import './App.scss';
-import { Link, Route, Routes, useNavigate } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import MainPage from './pages/mainPage/MainPage';
 import AdminPanelPage from './pages/AdminPanelPage/AdminPanelPage';
 import PostItem from './components/PostItem';
 import EditPage from './pages/AdminPanelPage/EditPage';
-import { useEffect } from 'react';
 
 
 function App() {
-  const navigate = useNavigate()
-  useEffect(() => {
-
-    navigate('/adminpanel')
-  }, [])
-
-
   return (
     <>
       <Routes>
         <Route path='/' element={<MainPage />} />
         <Route path='/adminpanel' element={<AdminPanelPage />} />
+        <Route path='/adminpanel-us' element={<Navigate to='/adminpanel' replace />} />
         <Route path='/edit/:id' element={<EditPage />} />
         <Route path='/post/:id' element={<PostItem />} />
       </Routes>
