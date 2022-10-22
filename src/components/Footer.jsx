56b22378b9f5a876dce60a6d07e2eac5b485d74d
creat1ve54 <100487936+creat1ve54@users.noreply.github.com>
2022-10-22 20:12:20 +0300

    load

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,15 +10,6 @@ const Footer = () => {
                     <li className='footer__item'>
                         <a href="mailto: informadm01@mail.ru" className="footer__link">informadm01@mail.ru</a>
                     </li>
-                    <li className='footer__item'>
-                        <a href="mailto: sergei.luzinov@mail.ru" className="footer__link">sergei.luzinov@mail.ru</a>
-                    </li>
-                    <li className='footer__item'>
-                        <a href="tel: +79183480675" className="footer__link">+7(918)348-06-75</a>
-                    </li>
-                    <li className='footer__item'>
-                        <a href="tel: +79628650219" className="footer__link">+7(962)865-02-19</a>
-                    </li>
                 </ul>
             </div>
         </div>
